4b2db1bf657bb898c965eb8143799cdc0f89a8e1
Anita Baral <baralanita2@gmail.com>
2021-08-07 17:55:28 +0545

    Looping through all the numerical columns to get the outliers percentage for each

--- statistics_and_outliers.py
+++ statistics_and_outliers.py
@@ -10,7 +10,7 @@ class CSVREADER:
         csv_reader = csv.reader(csv_file)
         return csv_reader
 
-    
+
     def head(self,i):
         self.i = i
         csv_reader = self.read_csv(self.filename)
@@ -25,16 +25,56 @@ class CSVREADER:
         for row in rows[:i]:
             print(','.join(item for item in row))
 
-        
+    def getColumns(self):
+        columns_numerical = defaultdict(list)
+        columns_categorical = defaultdict(list)
+        csv_reader = csv.DictReader(open(self.filename, 'r'))
+        for row in csv_reader:
+          for (k, v) in row.items():
+            if k in ['age', 'bmi', 'children', 'charges']:
+              columns_numerical[k].append(v)
+            else:
+              columns_categorical[k].append(v)
+        return columns_numerical, columns_categorical
 
     def shape(self):
         pass
+
+class Outliers:
+  def __init__(self, numerical_columns):
+    self.numerical_columns = numerical_columns
+    self.outliers_std = {}
+    self.outliers_zscore = {}
+
+  def stdOutlier(self):
+    for key, value in self.numerical_columns.items():
+      data = np.array(list(map(float, value)))
+      data_mean, data_std = np.mean(data), np.std(data)
+      cut_off = data_std * 3
+      lower, upper = data_mean - cut_off, data_mean + cut_off
+      outliers = [x for x in data if x < lower or x > upper]
+      self.outliers_std[key] = round(((len(outliers)/len(value)) * 100), 3)
+
+    return self.outliers_std
+
+  # identify outliers with interquartile range
+  def zscoreOutlier(self):
+    for key, value in self.numerical_columns.items():
+      data = np.array(list(map(float, value)))
+      q25, q75 = np.percentile(data, 25), np.percentile(data, 75)
+      iqr = q75 - q25
+      cut_off = iqr * 1.5
+      lower, upper = q25 - cut_off, q75 + cut_off
+      outliers = [x for x in data if x < lower or x > upper]
+      outliers_removed = [x for x in data if x >= lower and x <= upper]
+      self.outliers_zscore[key] = round(((len(outliers)/len(value)) * 100), 3)
+    return self.outliers_zscore
 
 
 pd = CSVREADER()
 pd.read_csv('diabetes.csv')
 pd.head(5)
 
-
-
-
+outliers = Outliers(cols_numerical)
+print(outliers.stdOutlier())
+print(outliers.zscoreOutlier())
